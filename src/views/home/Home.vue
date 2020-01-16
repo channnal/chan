52ae790b6d31419eae6c1e750abd5f6db7865d56
channnal <1148850672@qq.com>
2020-01-16 15:19:40 +0800

    chan

---
+++ src/views/home/Home.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+      <div class="header">
+        <h1>前端学习之路...</h1>
+      </div>
+
+      <div class="container">
+        <div class="catalog"></div>
+        <div class="content"></div>
+      </div>
+
+      <div class="footer"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {}
+    },
+    components: {},
+    methods: {}
+}
+</script>
+
+<style scoped>
+
+</style>
