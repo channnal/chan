48b60d4c1245baa01d5c37dafa4fce2fca341306
channnal <1148850672@qq.com>
2020-04-13 14:40:22 +0800

    vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    title: '个人主页', 
+    description: "channnal's blog",
+    head: [
+        ['link', { rel: 'icon', href: '/img/logo.ico' }],
+        ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ]
+}
