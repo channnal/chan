0e172a263cb2f8c69bd1cd6c96396dd5d6f3ab35
channnal <1148850672@qq.com>
2020-04-14 10:58:35 +0800

    'deploy'

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,18 +1,11 @@
 module.exports = {
     title: "channnal's blog", 
     description: "channnal's blog",
-    head: [
-        ['link', { rel: 'icon', href: '/' }],
-        ['link', { rel: 'manifest', href: '/' }],
-    ],
     themeConfig: {
         // 你的GitHub仓库，请正确填写
         repo: 'https://github.com/channnal/chan.git',
         // 自定义仓库链接文字。
         repoLabel: 'My GitHub',
-        nav: [
-            { text: 'Home', link: '/' },
-            { text: 'FirstBlog', link: '/' }
-        ]
+
     }
 }
